e56cd5562eed0d0bac2f540efa05b63a68b486e8
Dorde Drazic <djo52000@gmail.com>
2023-09-24 18:55:53 +0200

    Last
    Add Loading .... text for slow nerworks

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,38 @@ import { useState, useEffect } from "react"
 import { User } from "./User"
 
 function App() {
-    const [users, setUsers] = useState([])//if you not define as empty array you must apply line 17
+    const [isLoading, setIsLoading] = useState(true)
+    const [users, setUsers] = useState([])//if you not define as empty array you must apply line 21
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
+        setIsLoading(true)
+
+        const controller = new AbortController()
+        fetch('https://jsonplaceholder.typicode.com/users', { signal: controller.signal })
         .then(res => res.json())
         .then(setUsers)
-    })
+        .finally(() => {
+            setIsLoading(false)
+        })
+
+        return () => {
+            controller.abort()
+        }
+    }, [])
 
     return (
         <>
             <h1>Users List</h1>
-            <ul>{
-                // users != null && 
-                users.map(user => {
-                return <User name={user.name} />
-            })}
-            </ul>
+            {isLoading ? (
+                <h2>Loading...</h2>
+            ) : (
+                <ul>{
+                    // users != null && 
+                    users.map(user => {
+                    return <User key={ user.id } name={user.name} />
+                    })}
+                </ul>
+            )}
         </>
     )
 }
